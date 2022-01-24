e3df0e7be080cd6bf4cec5cde77409ee564ee1bd
Glubiz <62329544+Glubiz@users.noreply.github.com>
2022-01-24 16:06:07 +0100

    Delete controllers directory

--- controllers/main.js
+++
@@ -1,7 +0,0 @@
-//This file contains all the pages that can be loaded on the website
-exports.getIndex = (req, res, next) => {
-    res.render('main/index', {
-      pageTitle: 'Index',
-      path: '/'
-    });
-};
