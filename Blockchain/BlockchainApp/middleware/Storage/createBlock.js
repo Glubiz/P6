82dce87f025bd4ad92d34c498c7b6a49aa6690bf
Glubiz <62329544+Glubiz@users.noreply.github.com>
2022-02-15 09:01:09 +0100

    Add files via upload

--- Blockchain/BlockchainApp/middleware/Storage/createBlock.js
+++ Blockchain/BlockchainApp/middleware/Storage/createBlock.js
@@ -1,18 +1,21 @@
 const fs = require('fs')
 const hash = require('./hash')
-const nonce = require('./nonce')
+const calculateNonce = require('./nonce')
 const getPreviousBlock = require('./previousBlock')
 
 function createGenesis(chainID){
-    var fileName = chainID + '.json'
+    // Collects the server time in epoch format, this is done to get a consistant format for the time to add into the new block
     var dateTime = new Date().getTime().toString()
+
+    // Instantiate the new chain with the ID, and the timestamp. All the blocks are added to the data array in the block
     var chain = 
     {
         chainID : chainID,
-        instanciated : dateTime,
+        instantiated : dateTime,
         data : []
     }
 
+    // Loads the genesis data into the block with key value pairs to be ready to be sent to the blockchain
     var block = {
         'index' : 0, 
         'nonce' : 1,
@@ -22,37 +25,54 @@ function createGenesis(chainID){
         'timeStamp' : dateTime,
     }
 
+    // Adds the block to the chain
     chain.data.push(block)
 
-    fs.writeFileSync('./Blockchains/' + fileName, JSON.stringify(chain, null, 4))
+    // Creates and writes the blockchain to the json file belonging to the correct household
+    fs.writeFileSync('./Blockchains/' + chainID + '.json', JSON.stringify(chain, null, 4))
 }
 
 function createBlock(chainID, data) {
-    var fileName = chainID + '.json'
-
+    // Checks if the blockchain is created before adding the new block
     try {
-        if (fs.existsSync('./Blockchains/' + fileName)) {
-            var snapshot = fs.readFileSync('./Blockchains/' + fileName)
+        if (fs.existsSync('./Blockchains/' + chainID + '.json')) {
+            // Loads the previous chain as a json file to find the chain length and to be able to push the new block to the chain
+            var snapshot = fs.readFileSync('./Blockchains/' + chainID + '.json')
             var json = JSON.parse(snapshot)
             var chain = json
             var index = chain.data.length + 1
+
+            // Calls the getPreviousBlock function to collect the hash of the previous block
             var previousBlock = getPreviousBlock(chainID)
             var previousHash = previousBlock.hash
+            
+            // Calls the nonce function to get to calculate the nonce of the block and thereby making the block immutable
+            var nonce = calculateNonce(chainID, index)
+
+            // Collects the server time in epoch format, this is done to get a consistant format for the time to add into the new block
             var dateTime = new Date().getTime().toString()
 
+            // Loads the data into the block with key value pairs to be ready to be sent to the blockchain
             var block = {
                 'index' : index, 
-                'nonce' : nonce(chainID, index),
+                'nonce' : nonce,
                 'data' : data,
                 'hash' : hash(index.toString(), nonce, chainID, previousHash, dateTime.toString()),
                 'previousHash' : previousHash,
                 'timeStamp' : dateTime,
             }
+
+            // Adds the block to the chain
             chain.data.push(block)
 
-            fs.writeFileSync('./Blockchains/' + fileName, JSON.stringify(chain, null, 4))
+            // Writes the updated blockchain to the json file belonging to the correct household
+            fs.writeFileSync('./Blockchains/' + chainID + '.json', JSON.stringify(chain, null, 4))
         } else {
+            // Loads the genesis block, this is only done once
             createGenesis(chainID)
+
+            // Recalls the createBlock function to add the block containing the data to the newly created chain
+            createBlock(chainID, data)
         }
     } catch(err) {
         console.log(err)
@@ -61,6 +81,6 @@ function createBlock(chainID, data) {
     
 }
 
-createBlock("123adasw", "llsdlfsaagg")
+createBlock("123adasw", "gasfagsss")
 
 // module.exports = createBlock()
--- Blockchain/BlockchainApp/middleware/Storage/previousBlock.js
+++ Blockchain/BlockchainApp/middleware/Storage/previousBlock.js
@@ -1,12 +1,10 @@
 const fs = require('fs')
 
 module.exports = function getPreviousBlock(chainID){
-    var fileName = chainID + '.json'
-    var chain = JSON.parse(fs.readFileSync('./Blockchains/' + fileName))
-    if(chain.data.length > 1){
-        var previousBlock = chain.data[chain.data.length-1]
-    } else {
-        var previousBlock = chain.data[0]
-    }
+    // Loads the blockchain into the chain variable
+    var chain = JSON.parse(fs.readFileSync('./Blockchains/' + chainID + '.json'))
+
+    // The previousblock is equal to the length of the chain minus 1, since it is zero indexed
+    var previousBlock = chain.data[chain.data.length-1]
     return previousBlock
 }
