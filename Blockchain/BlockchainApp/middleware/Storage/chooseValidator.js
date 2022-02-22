517d0a8cce17132eff3191b409a1bdd3565bb5e0
Glubiz <62329544+Glubiz@users.noreply.github.com>
2022-02-22 16:19:05 +0100

    Validator V2

--- Blockchain/BlockchainApp/middleware/Storage/chooseValidator.js
+++ Blockchain/BlockchainApp/middleware/Storage/chooseValidator.js
@@ -1,13 +1,25 @@
 const fs = require('fs')
+var arr = []
 
 function findValidatorNode(){
     var chain = JSON.parse(fs.readFileSync('./Blockchains/' + 'kfkaksfkas' + '.json'))
-    var maxIndex = chain.data.length - 1
-    // Finds a random validator node
-    var index = Math.floor(Math.random() * parseInt(maxIndex))
-    console.log(index)
-    return index
+    var maxIndex = chain.data.length
+    // If the network of nodes (IoT gateways) is big enough, we want 10% of the network to validate the chain. the nodes that has been in the network the longest weighs the most on the proccess
+    // for (var i = 0; i < parseInt(maxIndex * 0.1); i++){
+    
+    arr = []
+    for (var i = 0; i < 3; i++){
+        // Finds a random validator node
+        var index = Math.floor(Math.random() * parseInt(maxIndex))
+        console.log(index)
+        if (arr.includes(index)){
+            i--
+        } else {
+            arr.push(index)
+        }
+    }
+    console.log(arr)
 }
 setInterval(findValidatorNode, 3000)
 
-module.exports = findValidatorNode()
+module.exports = [arr]
