3b741c71ee754f2624d4be25ec2f3b003d3a6fca
Glubiz <62329544+Glubiz@users.noreply.github.com>
2022-05-12 18:15:03 +0200

    Deleted api endpoints on iot device

--- IoT/routes/api.js
+++ IoT/routes/api.js
@@ -6,13 +6,4 @@ const Controller = require('../controllers/api');
 
 const router = express.Router();
 
-//Endpoints
-
-//POST
-router.post('/addNode', Controller.addNode);
-router.post('/fetchEventHash', Controller.addNode);
-
-//GET
-router.post('/Ping', Controller.Ping);
-
 module.exports = router;
